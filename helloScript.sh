5042b9b8ed4f52e04cabb5f5ae720f81eb8b5bf2
yukitya-1811 <adityapandia18@gmail.com>
2022-09-21 20:31:12 +0530

    TRUE FIRST COMMIT

--- helloScript.sh
+++ helloScript.sh
@@ -1,19 +1,11 @@
 #1 /bin/bash
 
-count=100
+count=9
 
-if [ $count > 9 ]
+if [ $count = 9 ]
 then
 	echo 'The first condition is true'
 
 else
 	echo 'The first condition is false'
-fi
 
-
-if [ $count < 20 ]
-then
-	echo 'The second condition is true'
-else
-	echo 'The second condition is false'
-fi

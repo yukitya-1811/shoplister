ff75b201344b77eadb95128699df48b773a50f0d
yukitya-1811 <adityapandia18@gmail.com>
2022-09-22 13:59:41 +0530

    ADDED inequality TO MASTER

---
+++ inequalityScript.sh
@@ -0,0 +1,20 @@
+#1 /bin/bash
+
+count=15
+
+if [ $count -gt 10 ]
+then
+	echo 'The 1st condition is true'
+
+else
+	echo 'The 1st condition is false'
+fi
+
+if [ $count -gt 20 ]
+then
+	echo 'The 2nd condition is true'
+
+else
+	echo 'The 2nd condition is false'
+
+fi
